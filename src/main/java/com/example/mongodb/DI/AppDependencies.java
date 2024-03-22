a8e1983120cc2068c668a68b626c7e8cf6b834ec
BABAHAJ Zakarya <z.babahaj@edu.umi.ac.ma>
2024-03-22 14:51:22 +0000

    add test comment

--- src/main/java/com/example/mongodb/DI/AppDependencies.java
+++ src/main/java/com/example/mongodb/DI/AppDependencies.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppDependencies {
 
-
+//test
     static  WalletRepository walletRepository;
     @Bean
     public static IWalletService dependenciInjenctionService(){
